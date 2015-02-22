95e1bdab758f6e8a1948a55a7718ef98e4f7f4ab
Paratuel <paratuelvenatir@gmail.com>
2015-02-22 16:36:03 -0500

    Update project.java

--- project.java
+++ project.java
@@ -1,1 +1,25 @@
+import java.util.Date;
+
+public class project {
+
+		String category;
+		String pname;		
+	    Date dueDate;		
+	    String notes;		
+						
+	    project(){		
+	    category = "Untitled";
+	    pname = "Untitled";		
+	    dueDate = new Date(1,1,2015);
+	    notes = "";		
+	   }		
+			
+	    project(String n, Date dd, String c, String nt){		
+	        pname = n;		
+	        dueDate = dd;	
+	        notes = nt;		
+	    }
+	
+	
+}
 
