cc966c499c93eff4b1bb32b74f645a8d0a9432d9
Paratuel <paratuelvenatir@gmail.com>
2015-02-22 16:36:21 -0500

    Update sub.java

--- sub.java
+++ sub.java
@@ -1,1 +1,25 @@
+import java.util.Date;
+
+public class sub {
+
+	String project;
+	String sname;
+	Date dueDate;
+	String notes;
+	
+	sub(){
+		project = "Untitled";
+		sname = "Untitled";
+		dueDate = new Date(1,1,2015);
+		notes = "";
+	}
+	
+	sub(String p, String sn, Date dd, String n){
+		project = p;
+		sname = sn;
+		dueDate = dd;
+		notes = n;
+	}
+	
+}
 
