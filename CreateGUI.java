ef6abc359a0365afc79ec8cedf22594ddf53129c
youngla1972 <youngla@mail.gvsu.edu>
2015-03-18 23:05:44 -0400

    Changed quite a bit.
    I've been changing how things are being passed around.  The main reason is I changed the GUI into a table form for ease of reading.

--- CreateGUI.java
+++ CreateGUI.java
@@ -14,142 +14,177 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.WindowConstants;
 
 public class CreateGUI extends JDialog implements ActionListener {
 
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-  private GridLayout layout;
+	private GridLayout layout;
 
-  private JButton okButton; 
-  private JButton cancelButton;
-  
-  private JLabel nameLabel;
-  private JLabel dateLabel;
-  private JLabel catLabel; 
-  private JLabel noteLabel;
-  private JLabel subLabel;
+	private JButton okButton; 
+	private JButton cancelButton;
 
-  private JTextField nameField;
-  private JTextField dateField;
-  private JTextField catField;
-  private JTextField noteField;
-  private JTextField subField;
+	private JLabel nameLabel;
+	private JLabel dateLabel;
+	//private JLabel catLabel; 
+	private JLabel noteLabel;
+	private JLabel subLabel;
 
-  private DateFormat format;
+	private JTextField nameField;
+	private JTextField dateField;
+	//private JTextField catField;
+	private JTextField noteField;
+	private JTextField subField;
 
-  private int WIDTH;
-  private int HEIGHT;
+	private DateFormat format;
 
-  private boolean isOk;
-  private boolean isSubOk;
+	private int WIDTH;
+	private int HEIGHT;
 
-  private project aProject;
-  
-  /**
-  * Sets up the panel for adding a project
-  * @param ProjectGUI is the parent GUI
-  * @param Project proj is passing in the array from ProjectGUI
-  */
-  public CreateGUI(projectGUI parent, project proj){
-    super(parent, true);
-    isOk = false;
-    isSubOk = true;
-    format = DateFormat.getDateInstance(DateFormat.SHORT);
-    aProject = proj;
-    WIDTH = 400;
-    HEIGHT = 400;
-    layout = new GridLayout(7,2);
-    okButton = new JButton("OK");
-    okButton.addActionListener(this); 
-    cancelButton = new JButton("Cancel");
-    cancelButton.addActionListener(this);
-    nameLabel = new JLabel("Name of Project:");
-    dateLabel = new JLabel("Due date (MM/DD/YYYY):");
-    catLabel = new JLabel("Category:");
-    noteLabel = new JLabel("Notes:");
-    subLabel = new JLabel("Number of sub projects:");
-    nameField = new JTextField(20);
-    nameField.setText(proj.getName());
-    dateField = new JTextField(10);
-    dateField.setText("10/10/2012");
-    catField = new JTextField(20);
-    catField.setText(proj.getCategory());
-    noteField = new JTextField(70);
-    noteField.setText(proj.getNotes());
-    subField = new JTextField(3);
-    subField.setText("0");
+	private boolean isOk;
+	private boolean cancel;
+	private boolean isSubOk;
+	private ProjectGUI parent;
 
-    add(nameLabel);
-    add(nameField);
-    add(dateLabel);
-    add(dateField);
-    add(catLabel);
-    add(catField);
-    add(noteLabel);
-    add(noteField);
-    add(subLabel);
-    add(subField);
-    add(cancelButton);
-    add(okButton);
+	private Project aProject;
 
-    setLayout(layout);
-    setSize(WIDTH, HEIGHT);
-    setLocationRelativeTo(null);
-    setTitle("New Project");
-    setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    setVisible(true); 
-  }
+	/**
+	 * Sets up the panel for adding a project
+	 * @param ProjectGUI is the parent GUI
+	 * @param Project proj is passing in the array from ProjectGUI
+	 */
+	public CreateGUI(ProjectGUI parent){
+		super(parent, true);
+		this.parent = parent;
+		setupDialog();
+		setTitle("Project Management");
+	}
+	
+	public void setupDialog(){
+		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		nameField = new JTextField(20);
+		dateField = new JTextField(10);
+		noteField = new JTextField(70);
+		subField = new JTextField(3);
+		
+		nameField.setText("Math Homework");
+		dateField.setText("10/10/2012");
+		noteField.setText("Take action NOW!");
+		subField.setText("0");
+		
+		isOk = false;
+		isSubOk = true;
+		format = DateFormat.getDateInstance(DateFormat.SHORT);
+		
+		WIDTH = 400;
+		HEIGHT = 400;
+		layout = new GridLayout(7,2);
+		okButton = new JButton("OK");
+		okButton.addActionListener(this); 
+		cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(this);
+		nameLabel = new JLabel("Name of Project:");
+		dateLabel = new JLabel("Due date (MM/DD/YYYY):");
+		//catLabel = new JLabel("Category:");
+		noteLabel = new JLabel("Notes:");
+		subLabel = new JLabel("Number of sub projects:");
+		
+		
+		//catField = new JTextField(20);
+		//catField.setText(proj.getCategory());
+		
 
-  /*
-  * Assigns actions to buttons and JMenuItems
-  */
-  public void actionPerformed(ActionEvent e) {
-    if (e.getSource() == cancelButton) {
-      dispose();
-    }
-    if (e.getSource() == okButton) {
-      try {
-        String newName = nameField.getText();
-        String newDateString = dateField.getText();
-        String newCat = catField.getText();
-        String newNote = noteField.getText();
-        Date newDate = format.parse(newDateString);
-        GregorianCalendar newDate2 = new GregorianCalendar();
-        newDate2.setTime(newDate);
-        aProject.setName(newName);
-        aProject.setDueDate(newDate2);
-        aProject.setNotes(newNote);
-        aProject.setCategory(newCat);
-        isOk = true;
-        dispose();
-        int num = Integer.parseInt(subField.getText());
-        while (num > 0) {
-          SubCreateGUI subCreate = new SubCreateGUI(this);
-          if (subCreate.isOkPressed()) {
-            aProject.addItems(subCreate.whatProject());
-          }
-          num--;
-        }
-      } catch (Exception x) {
-        x.printStackTrace();
-      }
-    }
-  }
-  
-  /*
-  * Checks status
-  * @return isOk if true
-  */
-  public boolean isOkPressed() {
-    return isOk;
-  }
-  
-  /*
-  * Checks which project
-  * @return aProject is the Project
-  */
-  public project whatProject() {
-    return aProject;
-  }
+		add(nameLabel);
+		add(nameField);
+		add(dateLabel);
+		add(dateField);
+		//add(catLabel);
+		//add(catField);
+		add(noteLabel);
+		add(noteField);
+		add(subLabel);
+		add(subField);
+		add(cancelButton);
+		add(okButton);
+
+		setLayout(layout);
+		setSize(WIDTH, HEIGHT);
+		setLocationRelativeTo(null);
+		setTitle("New Project");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setVisible(true); 
+	}
+
+	/*
+	 * Assigns actions to buttons and JMenuItems
+	 */
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == cancelButton) {
+			dispose();
+		}
+		if (e.getSource() == okButton) {
+			try {
+				String newName = nameField.getText();
+				String newDateString = dateField.getText();
+				//String newCat = catField.getText();
+				String newNote = noteField.getText();
+				Date newDate = format.parse(newDateString);
+				GregorianCalendar newDate2 = new GregorianCalendar();
+				newDate2.setTime(newDate);
+				aProject.setName(newName);
+				aProject.setDueDate(newDate2);
+				aProject.setNotes(newNote);
+				//aProject.setCategory(newCat);
+				isOk = true;
+				dispose();
+				int num = Integer.parseInt(subField.getText());
+				while (num > 0) {
+					SubCreateGUI subCreate = new SubCreateGUI(this);
+					if (subCreate.isOkPressed()) {
+						//aProject.addItems(subCreate.whatProject());
+					}
+					num--;
+				}
+			} catch (Exception x) {
+				x.printStackTrace();
+			}
+		}
+	}
+
+	/*
+	 * Checks status
+	 * @return isOk if true
+	 */
+	public boolean isOkPressed() {
+		return isOk;
+	}
+	public boolean isCancel(){
+		return cancel;
+	}
+	
+	public String getName(){
+		return nameField.getText();
+	}
+	public GregorianCalendar getDueDate(){
+		return Utilities.strToGregCalendar(dateField.getText());
+	}
+	public String getNotes(){
+		return noteField.getText();
+	}
+
+	/*
+	 * Checks which project
+	 * @return aProject is the Project
+	 */
+	public Project whatProject() {
+		return aProject;
+	}
+	
+	public void clear(){
+		nameField.setText(null);
+		dateField.setText(null);
+		noteField.setText(null);
+		subField.setText(null);
+	}
 }
