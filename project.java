429e6934e4a65851d96c189b1e7b5223732766e7
youngla1972 <youngla@mail.gvsu.edu>
2015-03-18 23:02:24 -0400

    added some gets and sets
    Frankly, after I worked on this for 5 hours tonight, I don't remember all I changed.

--- project.java
+++ project.java
@@ -1,7 +1,6 @@
 package package1;
 
 import java.io.Serializable;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -13,128 +12,130 @@ import java.util.Vector;
  * of projects needed to be done with the added benefit of subgroups.
  * Created by Patrick Dishaw, Laura Young, Viet Duong, Nicholas Bushen
  */
-public class project implements Serializable {
+public class Project implements Serializable {
 
-  private static final long serialVersionUID = 1L;
-  private String name;
-  private GregorianCalendar dueDate;
-  private String category;
-  private String notes;
-  private Vector<project> subTasks;
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private GregorianCalendar dueDate;
+	private int reminder;
+	private String notes;
+	private Vector<Project> subTasks;
 
-  ArrayList<project> Projects = new ArrayList<project>();
+	ArrayList<Project> Projects = new ArrayList<Project>();
+	
+	/**
+	 * Initializing needed variables.
+	 */
+	public Project() {
+		name = "Untitled";
+		dueDate = new GregorianCalendar(2015,1,1);
+		notes = "";
+		subTasks = new Vector<Project>();
+	}
 
-  /*
-  * Initializing needed variables.
-  */
-  project() {
-    name = "Untitled";
-    dueDate = new GregorianCalendar(2015,1,1);
-    category = "Untitled";
-    notes = "";
-    subTasks = new Vector<project>();
-  }
+	/**
+	 * Initializing needed variables that are passed into the construct
+	 * @param n is the name of the project
+	 * @param dd is the due date of the project
+	 * @param c is the category for the project
+	 * @param nt notes for the project
+	 */
+	public Project(String n, GregorianCalendar dd, String nt) {
+		this.name = n;
+		this.dueDate = dd;
+		//this.reminder = rr;
+		this.notes = nt;
+		subTasks = new Vector<Project>();
+	}
+	
+	
 
-  /**
-  * Initializing needed variables that are passed into the construct
-  * @param n is the name of the project
-  * @param dd is the due date of the project
-  * @param c is the category for the project
-  * @param nt notes for the project
-  */
-  project(String n, GregorianCalendar dd, String c, String nt) {
-    name = n;
-    dueDate = dd;
-    category = c;
-    notes = nt;
-    subTasks = new Vector<project>();
-  }
+	/**
+	 * Setting up the string that will print
+	 */  
+	public String toString() {
+		return name + " " + dueDate.get(Calendar.MONTH) + "/" + 
+				dueDate.get(Calendar.DAY_OF_MONTH) + "/" + 
+				dueDate.get(Calendar.YEAR) + " " + notes;
+	}
 
-  /**
-  * Setting up the string that will print
-  */  
-  public String toString() {
-	  SimpleDateFormat format = new SimpleDateFormat("MM/dd/YYYY");
-    return name + " " + format.format(dueDate.getTime()) + " " + category + " " + notes;
-  }
+	/**
+	 * Returns the name of the project
+	 * @return name of the project
+	 */
+	protected String getName() {
+		return name;
+	}
 
-  /**
-  * Returns the name of the project
-  * @return name of the project
-  */
-  protected String getName() {
-    return name;
-  }
+	/**
+	 * Sets the name of the Project object with the name being passed into it.
+	 * @param name is the project name.
+	 */
+	protected void setName(String name) {
+		this.name = name;
+	}
 
-  /**
-  * Sets the name of the Project object with the name being passed into it.
-  * @param name is the project name.
-  */
-  protected void setName(String name) {
-    this.name = name;
-  }
+	/**
+	 * Returns the due date of the project
+	 * @return dueDate of the project
+	 */
+	protected GregorianCalendar getDueDate() {
+		return dueDate;
+	}
 
-  /*
-  * Returns the due date of the project
-  * @return dueDate of the project
-  */
-  protected GregorianCalendar getDueDate() {
-    return dueDate;
-  }
-  
-  /**
-  * Sets the dueDate of the Project object with the dueDate passing into it.
-  * @param dueDate is the due date of the project
-  */
-  protected void setDueDate(GregorianCalendar dueDate) {
-    this.dueDate = dueDate;
-  }
+	/**
+	 * Sets the dueDate of the Project object with the dueDate passing into it.
+	 * @param dueDate is the due date of the project
+	 */
+	protected void setDueDate(GregorianCalendar dueDate) {
+		this.dueDate = dueDate;
+	}
+	
+	/**
+	 * Returns the reminder for the project
+	 * @return reminder of the project
+	 */
+	protected int getReminder(){
+		return reminder;
+	}
+	
+	/**
+	 * Sets the reminder of the Project object with the reminder passing into it.
+	 * @param reminder is the reminder for the Project
+	 */
+	protected void setReminder(int reminder){
+		this.reminder = reminder;
+	}
 
-  /**
-  * Returns the category of the project
-  * @return category is the category of the project
-  */
-  protected String getCategory() {
-    return category;
-  }
+	/**
+	 * Returns the notes of of the project
+	 * @return notes is the notes taken for the project
+	 */
+	protected String getNotes() {
+		return notes;
+	}
 
-  /**
-  * Sets up the category of the Project object with the category passing into it.
-  * @param category is the category of the project
-  */
-  protected void setCategory(String category) {
-    this.category = category;
-  }
+	/**
+	 * Sets up the notes of the Project object with the notes passing into it.
+	 * @param notes is the notes of the project
+	 */
+	protected void setNotes(String notes) {
+		this.notes = notes;
+	}
 
-  /**
-  * Returns the notes of of the project
-  * @return notes is the notes taken for the project
-  */
-  protected String getNotes() {
-    return notes;
-  }
+	/**
+	 * Adds subgroups into the subTasks vector with the subgroup being passed into it.
+	 * @param p is the subgroup of the project
+	 */
+	protected void addItems(Project p) {
+		subTasks.addElement(p);
+	}
 
-  /**
-  * Sets up the notes of the Project object with the notes passing into it.
-  * @param notes is the notes of the project
-  */
-  protected void setNotes(String notes) {
-    this.notes = notes;
-  }
-
-  /**
-  * Adds subgroups into the subTasks vector with the subgroup being passed into it.
-  * @param p is the subgroup of the project
-  */
-  protected void addItems(project p) {
-    subTasks.addElement(p);
-  }
-
-  /**
-  * Returns the Subtasks of the project
-  * @return subTasks are the subgroups of the project
-  */
-  protected Vector<project> getSubtasks() {
-    return subTasks;
-  }
+	/**
+	 * Returns the Subtasks of the project
+	 * @return subTasks are the subgroups of the project
+	 */
+	protected Vector<Project> getSubtasks() {
+		return subTasks;
+	}
 }
