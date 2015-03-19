1bb95754942815c02ea50502e1500921df11cde1
youngla1972 <youngla@mail.gvsu.edu>
2015-03-18 23:19:02 -0400

    Gregorian Calendar manipulation
    I had this with another project.  It contains ways of manipulating Gregorian calendar dates and I figured we can add something to this to help with reminders.  It is pretty well commented which method does what.  We can get rid of any methods we don't use.  I've already used 2 or them I think.

---
+++ Utilities.java
@@ -0,0 +1,95 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.swing.JOptionPane;
+
+/**
+ * The class where the dates are manipulated for information needed.
+ * @author lauray72
+ *
+ */
+public class Utilities {
+	/**
+	 * Makes a GregorianCalendar date into a string
+	 * @param i is the date being changed.
+	 * @return returns null if it is empty or the date in string form.
+	 */
+	public static String gToString(GregorianCalendar i){
+		if (i == null){
+			return "";
+		}
+		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+		return df.format(i.getTime());
+	}
+	
+	/**
+	 * Figures the amount of days between 2 GregorianCalendar dates
+	 * @param c1 is the first GregorianCalendar date
+	 * @param c2 is the second GregorialCalendar date
+	 * @return returns the number of days
+	 */
+	public static int daysLapsed(GregorianCalendar c1, GregorianCalendar c2){
+		int elapsed = 0, factor = 1;
+        GregorianCalendar gc1, gc2;
+        if (c2.after(c1)) {
+            gc1 = (GregorianCalendar) c1.clone();
+            gc2 = (GregorianCalendar) c2.clone();
+        } else {
+            gc1 = (GregorianCalendar) c2.clone();
+            gc2 = (GregorianCalendar) c1.clone();
+            factor = -1;
+        }
+        gc1.clear(Calendar.MILLISECOND);
+        gc1.clear(Calendar.SECOND);
+        gc1.clear(Calendar.MINUTE);
+        gc1.clear(Calendar.HOUR_OF_DAY);
+        
+        gc2.clear(Calendar.MILLISECOND);
+        gc2.clear(Calendar.SECOND);
+        gc2.clear(Calendar.MINUTE);
+        gc2.clear(Calendar.HOUR_OF_DAY);
+        
+        while (gc1.before(gc2)) {
+            gc1.add(Calendar.DATE, 1);
+            elapsed++;
+        }
+        return elapsed * factor;
+	}
+
+	/**
+	 * Makes a String date into a GregorianCalendar date
+	 * @param i is the string being changed.
+	 * @return returns the date in GregorianCalendar form
+	 */
+	public static GregorianCalendar strToGregCalendar(String i){
+		if (i == null){
+			return null;
+		}
+		GregorianCalendar g = new GregorianCalendar();
+		try {
+			DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+			Date date = df.parse(i);	
+			g.setTime(date);
+		} catch (ParseException ex) {
+			return null;
+		}
+		return g;
+	}
+	
+	/**
+	 * Finds out if the first GregorianCalendar comes before the second.
+	 * @param c1 is the first GregorianCalendar date
+	 * @param c2 is the second GregorianCalendar date
+	 * @return returns true of it comes before.
+	 */
+	public static boolean beforeAfter(GregorianCalendar c1, GregorianCalendar c2){
+		if (c1.after(c2)){
+			return false;
+		}
+		return true;
+	}
+}
