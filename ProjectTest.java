7adc4707e3ea5ad0e7525c75e9ebef8c6ec599e0
bushenn <nicholasbushen@Nicholass-MacBook-Pro.local>
2015-03-19 22:14:50 -0400

    WORKING JUnit tests
    Working on more, but these work!

--- ProjectTest.java
+++ ProjectTest.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -17,22 +18,29 @@ public class ProjectTest {
   @Test
   public void insertTest() {
 	  
-    GregorianCalendar d = new GregorianCalendar(1,1,2015);
+    GregorianCalendar d = new GregorianCalendar();
+    d.set(Calendar.MONTH, 1);
+    d.set(Calendar.DAY_OF_MONTH, 1);
+    d.set(Calendar.YEAR, 2015);
     Project p1 = new Project("Test1", d, "Note", new ArrayList<Project>());
-    Assert.assertEquals("Test1 Thu Jan 01 00:00:00 EST 2015 Test Note",
+    Assert.assertEquals("Test1 1/1/2015 Note",
     		p1.toString());
   }
   
   @Test
   public void insertTest2() {
 	  
-      GregorianCalendar d = new GregorianCalendar(1,1,2015);
+	  GregorianCalendar d = new GregorianCalendar();
+	  d.set(Calendar.MONTH, 1);
+	  d.set(Calendar.DAY_OF_MONTH, 1);
+	  d.set(Calendar.YEAR, 2015);
 	  Project p1 = new Project("Test1", d, "Fire", new ArrayList<Project>());
 	  Project p2 = new Project("Test2", d, "", new ArrayList<Project>());
 	  
-	  String test = p1.toString() + "'\n'" + p2.toString();
-	  Assert.assertEquals("Test1 Thu Jan 01 00:00:00 EST 2015 CIS 163 Fire\n"
-	  		+ "Test2 Thu Jan 01 00:00:00 EST 2015 CIS 350 ", test);
+	  String test = p1.toString() + "\n" + p2.toString();
+	  Assert.assertEquals("Test1 1/1/2015 Fire\nTest2 1/1/2015 ", test);
   }
-  
+  public void insertTest3(){
+	  
+  }
 }
