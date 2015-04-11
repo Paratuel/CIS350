ee5cae94412af9bf525bc609d602c98dea0ae761
Paratuel <paratuelvenatir@gmail.com>
2015-04-10 23:14:51 -0400

    Comments/Checkstyle and refresh method

--- ProjectModel.java
+++ ProjectModel.java
@@ -4,40 +4,60 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
-
-import javax.swing.AbstractListModel;
 import javax.swing.JOptionPane;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.JOptionPane;
+import java.io.FileNotFoundException;
 
+/**
+ * Class for the entire storage of our Project. 
+ * @author  Patrick Dishaw, Laura Young, Viet Duong, Nicholas Bushen
+ *
+ */
 public class ProjectModel extends AbstractTableModel implements Serializable {
 
+	/**
+	 * serialVersionUID. 
+	 */
 	private static final long serialVersionUID = 1L;
-	private ArrayList<Project> myArray;
-	private Project project;
-	private String[] columnNames = {"Project Name", "Sub-Groups", "Due Date", "Reminders", "Notes"};
 
 	/**
-	 * Initializing the Project object myArray
+	 * Array for storing each Project and its information.
+	 */
+	private ArrayList<Project> myArray;
+
+	/**
+	 * Project to store in the array.
+	 */
+	private Project project;
+
+	/**
+	 * String array for storing the JTable columnNames.
+	 */
+	private String[] columnNames = 
+		{"Project Name", "Sub-Projects", "Due Date", "Reminders", "Notes", "Complete"};
+
+	/**
+	 * Initializing the Project object myArray.
 	 */
 	public ProjectModel() {
 		myArray = new ArrayList<Project>();
 	}
 
 	/**
-	 * returns the columnNames length
+	 * returns the columnNames length.
 	 * @return columnNames length
 	 */
-	public int getColumnCount(){
+	public final int getColumnCount() {
 		return columnNames.length;
 	}
-	
+
 	/**
 	 * Returns the values for filling in the GUI table.
 	 * Object val holds the information to send.
@@ -45,20 +65,20 @@ public class ProjectModel extends AbstractTableModel implements Serializable {
 	 * @param col is the column that the information goes on
 	 * @return val.
 	 */
-	public Object getValueAt(int row, int col){
+	public final Object getValueAt(final int row, final int col) {
 		Object val = null;
-		switch(col){
+		switch(col) {
 		case 0:
-		//	if(myArray.get(row).getSubName() != null){
-		//		val = "";
-		//	}else{
+//			if (myArray.get(row).getSubName() != null) {
+//				val = "";
+//			} else {
 				val = myArray.get(row).getName();
-		//	}
+//			}
 			return val;
 		case 1:
-			if(myArray.get(row).getSubName() != null){
+			if (myArray.get(row).getSubName() != null) {
 				val = myArray.get(row).getSubName();
-			}else{
+			} else {
 				val = "";
 			}
 			return val;
@@ -69,79 +89,82 @@ public class ProjectModel extends AbstractTableModel implements Serializable {
 			val = myArray.get(row).getReminder();
 			return val;
 		case 4:
-			val = myArray.get(row).getNotes();	
+			val = myArray.get(row).getNotes();
+			return val;
+		case 5:
+			val = myArray.get(row).getDone();
 			return val;
 		default:
 			return val;
 		}
-		
+
 	}
-	
+
 	/**
-	 * retrieves the index of myArray
+	 * retrieves the index of myArray.
 	 * @param index is the position in myArray that is needed
 	 * @return the myArray at the index
 	 */
-	public Project get(int index){
+	public final Project get(final int index) {
 		return myArray.get(index);
 	}
-	
+
 	/**
-	 * Finds the index of a particular Project
+	 * Finds the index of a particular Project.
 	 * @param a is the Project it is asking for
 	 * @return the index of myArray at a
 	 */
-	public int indexOf(Project a){
+	public final int indexOf(final Project a) {
 		return myArray.indexOf(a);
 	}
-	
+
 	/**
-	 * Returns the myArray size
+	 * Returns the myArray size.
 	 * @return myArray.size() of the project
 	 * */
-	public int getSize() {
+	public final int getSize() {
 		return myArray.size();
 	}
 
 	/**
-	 * Returns an element from myArray
+	 * Returns an element from myArray.
 	 * @param index is the spot in the array needed.
 	 * @return myArray.get(index) is the element needed 
 	 */
-	public Object getElementAt(int index) {
+	public final Object getElementAt(final int index) {
 		return myArray.get(index);
 	}
 
 	/**
-	 * Adding an element to myArray with p being passed into it 
+	 * Adding an element to myArray with p being passed into it.
 	 * @param p is the Project object being passed into it. 
 	 */
-	public void add(Project p) {
+	public final void add(final Project p) {
 		myArray.add(p);
 		fireTableRowsInserted(0, myArray.size());
 	}
 
 	/**
-	 * Deleting a certain element in the myArray
+	 * Deleting a certain element in the myArray.
 	 * @param index is the position being passed in
 	 */
-	public void delete(int index) {
+	public final void delete(final int index) {
 		Object obj = myArray.remove(index);
 		fireTableRowsDeleted(0, myArray.size());
 		return;
 	}
-	
+
 	/**
 	 * retrieves the index for a particular Project.
 	 * @param a is the project.
 	 */
-	public void remove(Project a){
+	public final void remove(final Project a) {
 		delete(indexOf(a));
 	}
 	
-	public void upDate(String a, String b){
-		for (int i = 0; i < myArray.size(); i++){
-			if(myArray.get(i).getName().equals(a)){
+	public void upDate(String a, String b) {
+		for (int i = 0; i < myArray.size(); i++) {
+			if (myArray.get(i).getName().equals(a)) {
 				myArray.get(i).setName(b);
 				fireTableRowsUpdated(0, myArray.size());
 			}
@@ -149,36 +172,57 @@ public class ProjectModel extends AbstractTableModel implements Serializable {
 		
 		return;
 	}
+	
+	public void refresh() {
+		fireTableRowsUpdated(0, myArray.size());
+	}
+	
+
 	/**
-	 * returns the columnNames that will fill the table in the GUI
+	 * returns the columnNames that will fill the table in the GUI.
 	 * @param col sets the position for the columnNames on the table
 	 * @return columnNames and positions
 	 */
-	public String getColumnName(int col){
+	public final String getColumnName(final int col) {
 		return columnNames[col];
 	}
-	
-	public void sortByDate(){
-		if (myArray.size() > 1){
+
+	/**
+	 * Sorts array by the due date.
+	 * (Still to be implemented)
+	 */
+	public final void sortByDate() {
+		if (myArray.size() > 1) {
 			Collections.sort(myArray, new ProjectDateComparator());
 			this.fireTableRowsUpdated(0, myArray.size());
 		}
 	}
+	
 	/**
 	 * Sorts the listSites by Name and updates the GUI table.
 	 */
-	public void sortByName(){
-		if (myArray.size() > 1){
+	public final void sortByName() {
+		if (myArray.size() > 1) {
 			Collections.sort(myArray, new NameComparator());
 			this.fireTableRowsUpdated(0, myArray.size() - 1);
 		}
 	}
+	
+	public final void sortByWeek(int n) {
+		for (int i = 0; i < myArray.size(); i++) {
+				int x = Utilities.CurrentDateComp(myArray.get(i).getDueDate());
+				if (x <= (7 * n)) {
+					this.fireTableRowsUpdated(0, myArray.size() - 1);
+				}
+			
+		}
+	}
 
 	/**
-	 * Saving the file of the program
+	 * Saving the file of the program.
 	 * @param file being passed into it.
 	 */
-	public void save(File file) {
+	public final void save(final File file) {
 		try {
 			FileOutputStream fos = new FileOutputStream(file);
 			BufferedOutputStream bos = new BufferedOutputStream(fos);
@@ -187,16 +231,15 @@ public class ProjectModel extends AbstractTableModel implements Serializable {
 			oos.close();
 			bos.close();
 			fos.close();
-		} catch(Exception e) {
+		} catch (final Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 	/**
-	 * Loading the file of the program
 	 * @param file being passed into it.
 	 */
-	public void load(File file) {
+	public final void load(final File file) {
 		try {
 			FileInputStream fis = new FileInputStream(file);
 			BufferedInputStream bis = new BufferedInputStream(fis);
@@ -206,25 +249,25 @@ public class ProjectModel extends AbstractTableModel implements Serializable {
 			bis.close();
 			fis.close();
 			fireTableRowsInserted(myArray.size() - 1, myArray.size() - 1);
-		} catch (FileNotFoundException f){
+		} catch (FileNotFoundException f) {
 			save(file);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		for(int i = 0; i < myArray.size(); i++){
-			if(myArray.get(i).getReminder() != 0){
+		for (int i = 0; i < myArray.size(); i++) {
+			if (myArray.get(i).getReminder() != 0) {
 				int x = Utilities.CurrentDateComp(myArray.get(i).getDueDate());
-				if (x <= myArray.get(i).getReminder()){
-					JOptionPane.showMessageDialog(null, "Reminder for Project: " + 
-							myArray.get(i).getName());
-					
+				if (x <= myArray.get(i).getReminder()) {
+					JOptionPane.showMessageDialog(null, "Reminder for Project: "
+							+ myArray.get(i).getName());
+
 				}
 			}
 		}
 	}
 
 	@Override
-	public int getRowCount() {
+	public final int getRowCount() {
 		return myArray.size();
 	}
 }
