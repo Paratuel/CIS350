d8781470f5584d76120f9481075626a3aa221a68
Paratuel <paratuelvenatir@gmail.com>
2015-04-21 09:39:07 -0400

    Final Checkstyle edits

--- NameComparator.java
+++ NameComparator.java
@@ -1,23 +1,18 @@
-package package1;
+package packageproj;
 
 import java.util.Comparator;
 
 public class NameComparator implements Comparator<Project> {
-	/**
-	 * compares the names
-	 * @param s1 is a name
-	 * @param s2 is a name
-	 * @return returns results
-	 */
-	
-	public int compare(Project p1, Project p2) {
-		if(p1.getName().equals(p2.getName()) && p1.getSubName() == null){
-			return -1;
-		}
-		if(p1.getName().equals(p2.getName()) && p2.getSubName() == null){
-			return 1;
-		}
-		return p1.getName().compareTo(p2.getName());
-	}
+  /**
+   * compares the names.
+   * @param s1 is a name
+   * @param s2 is a name
+   * @return returns results
+   */
+  
+  @Override
+  public int compare(Project o1, Project o2) {
+    return o1.getName().compareTo(o2.getName());
+  }
 
 }
