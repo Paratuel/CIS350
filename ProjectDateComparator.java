e2f431ea3c366a746148751deff684c60dc10b6a
Paratuel <paratuelvenatir@gmail.com>
2015-04-21 09:39:34 -0400

    Final Checkstyle edits

--- ProjectDateComparator.java
+++ ProjectDateComparator.java
@@ -1,24 +1,23 @@
-package package1;
+package packageproj;
 
 import java.util.Comparator;
 
-public class ProjectDateComparator implements Comparator<Project>{
-	
-	/**
-	 * compares the dates
-	 * @param p1 is the first date
-	 * @param p2 is the second date
-	 * @return the results
-	 */
-	@Override
-	public int compare(Project p1, Project p2){
-		if(p2.getDone()){
-			return -1;
-		}
-		if(p1.getDone()){
-			return 1;
-		}
-
-		return p1.getDueDate().compareTo(p2.getDueDate());
-	}
+public class ProjectDateComparator implements Comparator<Project> {
+  
+  /**
+   * Compares the dates.
+   * @param p1 is the first date
+   * @param p2 is the second date
+   * @return the results
+   */
+  @Override
+  public int compare(Project p1, Project p2) {
+    if (p1.getName().equals(p2.getName()) && p1.getSubName() == null) {
+      return -1;
+    }
+    if (p1.getName().equals(p2.getName()) && p2.getSubName() == null) {
+      return 1;
+    }
+    return p1.getDueDate().compareTo(p2.getDueDate());
+  }
 }
