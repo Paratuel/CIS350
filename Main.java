9599783b1dba94d1a9a02cdeda9716ef0316b59c
Paratuel <paratuelvenatir@gmail.com>
2015-04-21 09:40:30 -0400

    Final Checkstyle edits

---
+++ Main.java
@@ -0,0 +1,12 @@
+package packageproj;
+
+  /**
+  * Main method to run Project Organization.
+  */
+public class Main {
+
+  public static void main(String[] args) {
+    new ProjectGui();
+    
+  }
+}
